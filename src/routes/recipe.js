7f07043690ffc04d6ec6f41b2f6a90893b3fecdd
Lexx Damm <lexx.damm@gmail.com>
2021-12-13 11:46:00 +0100

    getRecipesByUser return all recipes by userId

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { getAllRecipes, getRecipe, postRecipe, deleteRecipe } from '../controllers/recipe';
+import {
+    getAllRecipes,
+    getRecipe,
+    postRecipe,
+    deleteRecipe,
+    getRecipesByUser
+} from '../controllers/recipe';
 
 const router = express.Router();
 
 router.post('/', postRecipe);
 router.get('/', getAllRecipes);
 router.get('/:recipeId', getRecipe);
+router.get('/user/:userId', getRecipesByUser);
 router.delete('/:recipeId', deleteRecipe);
 
 export default router;
